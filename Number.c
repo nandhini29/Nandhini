c761a94d2b23c1faa897ca1dd69c832018bcc0f5
nandhini29 <30343474+nandhini29@users.noreply.github.com>
2017-07-22 16:47:31 +0530

    Create Number.c

---
+++ Number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the number\n");
+scanf("%d",&a);
+if(a%==0)
+{
+printf("The given number is even\n");
+}
+else
+{
+printf("The given number is odd\n");
+}
+getch();
+}
