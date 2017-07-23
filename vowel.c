5a7daa8da6fbdf0c4184a65fa25898aadd848341
nandhini29 <30343474+nandhini29@users.noreply.github.com>
2017-07-23 16:56:59 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+printf(" enter the alphabet \n");
+scanf("%c",&a);
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+printf("the given alphabet is vowel\n");
+else
+printf("the given alphabet is constant \n");
+return 0;
+}
