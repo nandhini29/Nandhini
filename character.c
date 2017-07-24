0c3b87c775eedb4bf89314a1b0ee9282f12c23ad
nandhini29 <30343474+nandhini29@users.noreply.github.com>
2017-07-24 10:48:06 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+printf("enter the character\n");
+scanf("%c",&a);
+if((a>='a' && a<='z')||(a>='A' && a<='Z'))
+printf("%a ia an alphabet",a)
+else
+printf("%c is not an alphabet",a);
+return 0;
+}
